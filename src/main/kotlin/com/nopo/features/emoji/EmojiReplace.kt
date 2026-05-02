7c35c877a0169f0e508dbecb6cd6ee426378711f
nopo <nopotheemail@gmail.com>
2026-05-03 09:15:18 +1000

    unused imports

--- src/main/kotlin/com/nopo/features/emoji/EmojiReplace.kt
+++ src/main/kotlin/com/nopo/features/emoji/EmojiReplace.kt
@@ -1,17 +1,13 @@
 package com.nopo.features.emoji
 
 import com.google.common.reflect.TypeToken
-import com.google.gson.Gson
-import com.google.gson.stream.JsonReader
-import com.nopo.module.Module
 import com.nopo.NopoMod
+import com.nopo.module.Module
 import com.nopo.utils.Utils
 import com.nopo.utils.Utils.replace
 import net.fabricmc.fabric.api.client.message.v1.ClientReceiveMessageEvents
-import net.fabricmc.loader.api.FabricLoader
 import net.minecraft.network.chat.Component
 import java.lang.reflect.Type
-import java.nio.file.Files
 
 object EmojiReplace : Module("chatEmojis", NopoMod.config.chatEmojis) {
 
