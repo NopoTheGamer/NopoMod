51f29f27c5346689a5affef276a134c9cb12019a
NopoTheGamer <nopotheemail@gmail.com>
2026-05-12 11:50:35 +1000

    use actual chat events from the mod

--- src/main/kotlin/com/nopo/features/WokeName.kt
+++ src/main/kotlin/com/nopo/features/WokeName.kt
@@ -1,18 +1,20 @@
 package com.nopo.features
 
 import com.nopo.NopoMod
+import com.nopo.events.ModifyChat
 import com.nopo.module.FeatureModule
 import com.nopo.utils.Utils
 import com.nopo.utils.Utils.componentBuilder
 import com.nopo.utils.Utils.replace
-import net.fabricmc.fabric.api.client.message.v1.ClientReceiveMessageEvents
+import net.minecraft.network.chat.Component
 import java.awt.Color
 
-object WokeName : FeatureModule("woke", NopoMod.config.wokeConfig, shouldBeHidden = { !Utils.isDevAllowed() }) {
+object WokeName : FeatureModule("woke", NopoMod.config.wokeConfig, shouldBeHidden = { !Utils.isDevAllowed() }),
+    ModifyChat {
 
     val nameComp by lazy {
         componentBuilder {
-            append(Utils.createGradientText( Color(85, 255, 255), Color.MAGENTA, "Throwpo"))
+            append(Utils.createGradientText(Color(85, 255, 255), Color.MAGENTA, "Throwpo"))
             // TODO: remove on 26.1
             if (Utils.isDevAllowed()) {
                 append(" ")
@@ -23,12 +25,14 @@ object WokeName : FeatureModule("woke", NopoMod.config.wokeConfig, shouldBeHidde
         }
     }
 
-    init {
-        ClientReceiveMessageEvents.MODIFY_GAME.register { component, actionBar ->
-            if (actionBar || !config.enabled) return@register component
-            if (!component.string.contains("Throwpo")) return@register component
-            val comp = component.copy().replace("Throwpo", nameComp) ?: return@register component
-            return@register comp
-        }
+    override fun onModifyChat(
+        message: Component,
+        actionBar: Boolean
+    ): Component? {
+        if (actionBar || !config.enabled) return null
+        if (!message.string.contains("Throwpo")) return null
+        val comp = message.copy().replace("Throwpo", nameComp) ?: return null
+        return comp
     }
+
 }
