3b89f19d6cba4fafa83c93ec69ddf567428aac1c
nopo <nopotheemail@gmail.com>
2026-05-19 22:52:55 +1000

    add the ability to remote disable features

--- src/main/kotlin/com/nopo/data/Data.kt
+++ src/main/kotlin/com/nopo/data/Data.kt
@@ -9,4 +9,5 @@ data class Data(
     @Expose val devs: List<UUID>? = emptyList(),
     @Expose val emojisName: Map<UUID, List<String>>? = emptyMap(),
     @Expose val updateNotification: UpdateNotificationData? = UpdateNotificationData(NopoMod.CURRENT_VERSION, ""),
+    @Expose val disabledFeatures: List<String>? = emptyList()
 )
--- src/main/kotlin/com/nopo/module/FeatureModule.kt
+++ src/main/kotlin/com/nopo/module/FeatureModule.kt
@@ -2,8 +2,10 @@ package com.nopo.module
 
 import com.github.stivais.commodore.Commodore
 import com.google.gson.annotations.Expose
+import com.nopo.NopoMod
 import com.nopo.config.ConfigManager
 import com.nopo.config.ModuleConfig
+import com.nopo.utils.DelayedRuns
 import com.nopo.utils.Utils
 import com.nopo.utils.Utils.withColor
 import net.minecraft.ChatFormatting
@@ -15,6 +17,19 @@ open class FeatureModule(
 ) : BaseModule(moduleName, shouldBeHidden) {
 
     open fun registerToggleCommand(): Commodore? {
+        if (!Utils.isDevAllowed()) {
+            val disabledFeatures = NopoMod.data?.disabledFeatures ?: emptyList()
+            if (moduleName in disabledFeatures) {
+                if (config.enabled) {
+                    DelayedRuns.schedule(100) {
+                        Utils.sendMessageToPlayer("$moduleName has been remotely disabled :(")
+                        ConfigManager.save()
+                    }
+                }
+                config.enabled = false
+                return null
+            }
+        }
         if (shouldBeHidden()) return null
         return Commodore("nopo") {
             literal("feature") {
