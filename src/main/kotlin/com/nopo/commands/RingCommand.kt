1135a5ef2737a541d77c62975a29d1d11c08c846
nopo <nopotheemail@gmail.com>
2026-06-29 21:55:59 +1000

    add ring command

---
+++ src/main/kotlin/com/nopo/commands/RingCommand.kt
@@ -0,0 +1,45 @@
+package com.nopo.commands
+
+import com.github.stivais.commodore.Commodore
+import com.nopo.events.CommandRegistration
+import com.nopo.events.TickEvent
+import com.nopo.module.BaseModule
+import com.nopo.utils.Utils
+
+object RingCommand : BaseModule("ring command"), CommandRegistration, TickEvent {
+
+    private var ticks = 0
+    private var currentPersonToAnnoy: String? = null
+
+    override fun createCommand(): Commodore {
+        return Commodore("nopo") {
+            "ring" {
+                runs { name: String? ->
+                    if (name == null) {
+                        Utils.sendMessageToPlayer("You gotta ring someone :/")
+                        return@runs
+                    }
+                    currentPersonToAnnoy = name
+                }
+            }
+        }
+    }
+
+    override fun onTick(totalTicks: Int) {
+        if (currentPersonToAnnoy == null) {
+            ticks = 0
+            return
+        }
+        ticks++
+        if (ticks == 5) {
+            Utils.sendCommandToServer("w $currentPersonToAnnoy ✆ RING...")
+        }
+        if (ticks == 17) {
+            Utils.sendCommandToServer("w $currentPersonToAnnoy ✆ RING... RING...")
+        }
+        if (ticks == 29) {
+            Utils.sendCommandToServer("w $currentPersonToAnnoy ✆ RING... RING... RING...")
+            currentPersonToAnnoy = null
+        }
+    }
+}
