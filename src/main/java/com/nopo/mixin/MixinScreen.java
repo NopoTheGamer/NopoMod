0b2c227de983318b6a0876c6c7530c59ddb1999b
nopo <nopotheemail@gmail.com>
2026-07-15 15:52:27 +1000

    add an event to render stuff in screens

--- src/main/java/com/nopo/mixin/MixinScreen.java
+++ src/main/java/com/nopo/mixin/MixinScreen.java
@@ -1,11 +1,16 @@
 package com.nopo.mixin;
 
+import com.nopo.NopoMod;
+import com.nopo.events.ScreenRendering;
 import com.nopo.features.inventory.WardrobeKeybinds;
+import com.nopo.module.BaseModule;
+import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.input.KeyEvent;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Screen.class)
@@ -14,6 +19,15 @@ public class MixinScreen {
     public void keyPressed(KeyEvent keyEvent, CallbackInfoReturnable<Boolean> cir) {
         if (WardrobeKeybinds.onKeyPress((Screen) (Object) this)) {
             cir.setReturnValue(true);
+        }
+    }
+
+    @Inject(method = "renderWithTooltipAndSubtitles", at = @At("TAIL"))
+    public void onRender(GuiGraphics guiGraphics, int x, int y, float delta, CallbackInfo ci) {
+        for (BaseModule module : NopoMod.INSTANCE.getModules()) {
+            if (module instanceof ScreenRendering event) {
+                event.renderAfterScreen(guiGraphics);
+            }
         }
     }
 }
